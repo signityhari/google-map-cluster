53d994bf62d592ad2025569bf5492f4940040b59
Meghana <meghana@signitysolutions.com>
2024-08-01 13:54:59 +0530

    initial commit

---
+++ lib/src/cluster_item.dart
@@ -0,0 +1,11 @@
+import 'package:google_maps_cluster_manager/google_maps_cluster_manager.dart';
+import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart'
+    as map_interface;
+
+mixin ClusterItem {
+  map_interface.LatLng get location;
+
+  String? _geohash;
+  String get geohash => _geohash ??=
+      Geohash.encode(location, codeLength: ClusterManager.precision);
+}
